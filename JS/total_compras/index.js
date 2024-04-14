8fadf304f808d08f470becc24b0af6cbb3f5f2ad
Douglas Terrero <dougsst1@gmail.com>
2024-04-14 19:22:18 -0300

    organização_pastas

---
+++ JS/total_compras/index.js
@@ -0,0 +1,7 @@
+import itensCarrinho from './dadosCarrinho.js';
+import somarValor from './calculaValorCompra.js';
+
+const valorAPagar = itensCarrinho.reduce(somarValor, 0)
+;
+
+console.log(valorAPagar);
